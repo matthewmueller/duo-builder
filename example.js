2ee77e030101b2ab37dfcaa1cd2d412c9be8a047
Matthew Mueller <mattmuelle@gmail.com>
2014-03-31 01:05:29 -0300

    add 'duo-jade' to example

--- example.js
+++ example.js
@@ -1,27 +1,20 @@
 var Builder = require('./');
 var co = require('co');
-var jade = require('jade');
+var jade = require('duo-jade');
 var entry = __dirname + '/example-app/index.js';
+
 var builder = Builder(entry)
   .to('build/build.js')
-  .use('.jade', compileJade);
+  .transform('jade', jade());
 
 // wrap
 builder.build = co(builder.build);
 
 console.time('builder');
+
+// build
 builder.build(function(err) {
   if (err) throw err;
   console.timeEnd('builder');
   console.log('built!');
 });
-
-var fs = require('fs');
-var path = require.resolve('jade/runtime.js');
-var runtime = fs.readFileSync(path, 'utf8');
-builder.include('jade-runtime', runtime)
-
-function compileJade(src, json) {
-  return 'var jade = require(\'jade-runtime\');\n\n' +
-         'module.exports = ' + jade.compileClient(src) + ';';
-}
